07efc93e31a3c63bc28400deb674506c337357ee
kwren96 <katherine.wren196@gmail.com>
2019-10-22 20:15:07 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,186 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MailchimpSubscribe from "react-mailchimp-subscribe";
+import Button from "react-bootstrap/Button";
+import "./App.css";
+import Popup from "reactjs-popup";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const url =
+  "https://googlemail.us20.list-manage.com/subscribe/post?u=448f77c8350ec8c06bfcb0917&amp;id=052d77d211";
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showButtons: false
+    };
+  }
+
+  whatToRender = () => {
+    if (this.state.showButtons !== false) {
+      return (
+        <div className="rating-buttons">
+          <Popup
+            trigger={<div className="rating-button-warning">5</div>}
+            modal
+            closeOnDocumentClick
+          >
+            <div className="subscribe-info">
+              <img src={require("./clarity-customer.png")} />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <h2>
+                If you would pay £50 pcm to use this tool please enter your
+                email below
+              </h2>
+              <MailchimpSubscribe url={url} />
+            </div>
+          </Popup>
+          <Popup
+            trigger={<div className="rating-button-safe">80</div>}
+            modal
+            closeOnDocumentClick
+          >
+            <div className="subscribe-info">
+              <img src={require("./clarity-customer.png")} />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <h2>
+                If you would pay £50 pcm to use this tool please enter your
+                email below
+              </h2>
+              <MailchimpSubscribe url={url} />
+            </div>
+          </Popup>
+          <Popup
+            trigger={<div className="rating-button-warning">5</div>}
+            modal
+            closeOnDocumentClick
+          >
+            <div className="subscribe-info">
+              <img src={require("./clarity-customer.png")} />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <h2>
+                If you would pay £50 pcm to use this tool please enter your
+                email below
+              </h2>
+              <MailchimpSubscribe url={url} />
+            </div>
+          </Popup>
+          <Popup
+            trigger={<div className="rating-button-safe">98</div>}
+            modal
+            closeOnDocumentClick
+          >
+            <div className="subscribe-info">
+              <img src={require("./clarity-customer.png")} />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <h2>
+                If you would pay £50 pcm to use this tool please enter your
+                email below
+              </h2>
+              <MailchimpSubscribe url={url} />
+            </div>
+          </Popup>
+          <Popup
+            trigger={<div className="rating-button-mid">50</div>}
+            modal
+            closeOnDocumentClick
+          >
+            <div className="subscribe-info">
+              <img src={require("./clarity-customer.png")} />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <h2>
+                If you would pay £50 pcm to use this tool please enter your
+                email below
+              </h2>
+              <MailchimpSubscribe url={url} />
+            </div>
+          </Popup>
+          <Popup
+            trigger={<div className="rating-button-warning">10</div>}
+            modal
+            closeOnDocumentClick
+          >
+            <div className="subscribe-info">
+              <img src={require("./clarity-customer.png")} />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <h2>
+                If you would pay £50 pcm to use this tool please enter your
+                email below
+              </h2>
+              <MailchimpSubscribe url={url} />
+            </div>
+          </Popup>
+          <Popup
+            trigger={<div className="rating-button-mid">50</div>}
+            modal
+            closeOnDocumentClick
+          >
+            <div className="subscribe-info">
+              <img src={require("./clarity-customer.png")} />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <h2>
+                If you would pay £50 pcm to use this tool please enter your
+                email below
+              </h2>
+              <MailchimpSubscribe url={url} />
+            </div>
+          </Popup>
+        </div>
+      );
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="clarity-button">
+            <Button
+              variant="warning"
+              className="btn-lg"
+              onClick={e => this.setState({ showButtons: true })}
+            >
+              Clarity
+            </Button>
+            {this.whatToRender()}
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App;
